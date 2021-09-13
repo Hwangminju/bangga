94b029413178189398f8c19cf9848788a5122a0c
Min Ju Hwang <33114822+Hwangminju@users.noreply.github.com>
2021-09-13 23:17:54 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ var room = 'foo';
 // Could prompt for room name:
 // room = prompt('Enter room name:');
 
-var socket = io.connect('https://localhost:3000');
+var socket = io.connect('https://192.168.0.10:3000');
 
 if (room !== '') {
   socket.emit('create or join', room);
